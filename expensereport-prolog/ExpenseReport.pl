958604dcdd7588e85ffa0a5d3bdb906ba9b64b05
Christian Hujer <Christian.Hujer@nelkinda.com>
2021-09-29 10:20:18 +0530

    Add OCP violations.

--- expensereport-prolog/ExpenseReport.pl
+++ expensereport-prolog/ExpenseReport.pl
@@ -1,30 +1,26 @@
-expenseType(dinner, 'Dinner', 5000).
-expenseType(breakfast, 'Breakfast', 1000).
-expenseType(car_rental, 'Car Rental', 2000000000).
-isMeal(dinner).
-isMeal(breakfast).
-
 expense(Type, Amount, [Type, Amount]).
-mealAmount(Type, Amount, Result) :- isMeal(Type), Result is Amount.
-mealAmount(_, _, Result) :- Result is 0.
 
-print_report2([X|Y], Meals, Total) :-
+print_report([X|Y], Meals, Total) :-
     expense(Type, Amount, X),
-    expenseType(Type, ExpenseName, Limit),
-    mealAmount(Type, Amount, MealAmount),
-    format('~a\t~a\t', [ExpenseName, Amount]),
-    ( Amount > Limit -> writeln('X'); writeln(' ')),
-    NMeals is Meals + MealAmount,
-    NTotal is Total + Amount,
-    print_report2(Y, NMeals, NTotal).
+    ((Type = dinner; Type = breakfast) -> NMeals is Meals + Amount; NMeals is Meals),
 
-print_report2([], Meals, Total) :-
+    (Type = dinner -> Name = 'Dinner';
+    Type = breakfast -> Name = 'Breakfast';
+    Type = car_rental -> Name = 'Car Rental'),
+
+    ((Type = dinner, Amount > 5000; Type = breakfast, Amount > 1000) -> Marker = 'X'; Marker = ' '),
+
+    format('~a\t~a\t~a~n', [Name, Amount, Marker]),
+    NTotal is Total + Amount,
+    print_report(Y, NMeals, NTotal).
+
+print_report([], Meals, Total) :-
     format('Meal expenses: ~a~n', [Meals]),
     format('Total expenses: ~a~n', [Total]).
 
 print_report(X) :-
     format('Expenses: ~a~n', ['']),
-    print_report2(X, 0, 0).
+    print_report(X, 0, 0).
 
 main() :-
     print_report([
