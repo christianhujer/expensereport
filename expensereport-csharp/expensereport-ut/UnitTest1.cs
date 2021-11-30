eb71318a48dca57c9da04ecdd33773944a2e47cc
arlobelshee <arlo@belshee.com>
2021-11-30 10:57:38 -0500

    F - Switch to XUnit.

--- expensereport-csharp/expensereport-ut/UnitTest1.cs
+++ expensereport-csharp/expensereport-ut/UnitTest1.cs
@@ -1,18 +1,14 @@
-using NUnit.Framework;
+using System;
+using Xunit;
 
 namespace Tests
 {
-    public class Tests
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
+        [Fact]
         public void Test1()
         {
-            Assert.Pass();
+
         }
     }
 }
