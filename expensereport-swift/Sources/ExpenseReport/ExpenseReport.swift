53e4af87524152fab8accd6fcb84121f619e024e
Jon Reid <jon@qualitycoding.org>
2021-12-07 18:45:26 -0800

    F Compare against implied enum cases

--- expensereport-swift/Sources/ExpenseReport/ExpenseReport.swift
+++ expensereport-swift/Sources/ExpenseReport/ExpenseReport.swift
@@ -17,7 +17,7 @@ class ExpenseReport {
         var total = 0
         print("Expense Report \(Date())")
         for expense in expenses {
-            if (expense.type == ExpenseType.dinner || expense.type == ExpenseType.breakfast) {
+            if (expense.type == .dinner || expense.type == .breakfast) {
                 mealExpenses += expense.amount
             }
 
@@ -28,7 +28,7 @@ class ExpenseReport {
             case .carRental: expenseName = "Car Rental"
             }
 
-            let mealOverExpensesMarker = expense.type == ExpenseType.dinner && expense.amount > 5000 || expense.type == ExpenseType.breakfast && expense.amount > 1000 ? "X" : " "
+            let mealOverExpensesMarker = expense.type == .dinner && expense.amount > 5000 || expense.type == .breakfast && expense.amount > 1000 ? "X" : " "
             print("\(expenseName)\t\(expense.amount)\t\(mealOverExpensesMarker)")
 
             total += expense.amount
