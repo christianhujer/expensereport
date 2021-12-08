5458d2ebee951a8aa625a44f23b1491c324002c8
Jon Reid <jon@qualitycoding.org>
2021-12-07 18:38:09 -0800

    B!! Fix Swift errors

--- expensereport-swift/Sources/ExpenseReport/ExpenseReport.swift
+++ expensereport-swift/Sources/ExpenseReport/ExpenseReport.swift
@@ -13,28 +13,27 @@ struct Expense {
 
 class ExpenseReport {
     func printReport(expenses: [Expense]) {
-        let mealExpenses = 0
-        let total = 0
-        print("Expense Report " + Date())
+        var mealExpenses = 0
+        var total = 0
+        print("Expense Report \(Date())")
         for expense in expenses {
             if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
                 mealExpenses += expense.amount
             }
 
-            let expenseName = ""
+            var expenseName = ""
             switch (expense.type) {
-            case BREAKFAST: expenseName = "Breakfast"
-            case DINNER: expenseName = "Dinner"
-            case CAR_RENTAL: expenseName = "Car Rental"
+            case .BREAKFAST: expenseName = "Breakfast"
+            case .DINNER: expenseName = "Dinner"
+            case .CAR_RENTAL: expenseName = "Car Rental"
             }
 
             let mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " "
-            print(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker)
+            print("\(expenseName)\t\(expense.amount)\t\(mealOverExpensesMarker)")
 
             total += expense.amount
         }
-        print("Meal Expenses: " + mealExpenses)
-        print("Total Expenses:" + total)
-        print(generateReport(expenses, Date()), terminator: "")
+        print("Meal Expenses: \(mealExpenses)")
+        print("Total Expenses:\(total)")
     }
 }
