bce6bc0b6b897f44e762cceaa0fcbe4eda0ae74a
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-02-03 19:59:22 +0530

    Add facility to use Cucumber instead of JUnit for Kotlin and Java.

---
+++ expensereport-java/src/test/java/com/nelkinda/training/CucumberTest.java
@@ -0,0 +1,12 @@
+package com.nelkinda.training;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@SuppressWarnings("java:S2187")
+public class CucumberTest {
+}
---
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/CucumberTest.kt
@@ -0,0 +1,10 @@
+package com.nelkinda.training
+
+import org.junit.platform.suite.api.IncludeEngines
+import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+class CucumberTest
