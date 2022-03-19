8d2e42f2da12c7a159ca36a56447c8005d7b1c8b
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-03-19 12:43:06 +0530

    Update Kotlin solution to include a test with Cucumber.

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
@@ -4,7 +4,7 @@ import java.lang.Integer.MAX_VALUE
 import java.util.Date
 
 enum class ExpenseType(
-    val name_: String,
+    val expenseName: String,
     val limit: Int,
     val isMeal: Boolean,
 ) {
@@ -19,23 +19,24 @@ class Expense(
     private val type: ExpenseType,
     val amount: Int,
 ) {
-    val name: String get() = type.name_
+    val name: String get() = type.expenseName
     val isMeal: Boolean get() = type.isMeal
     fun isOverLimit() = amount > type.limit
 }
 
 class ExpenseReport {
-    fun printReport(expenses: List<Expense>, timestamp: Date = Date()) = print(expenses.generateReport(timestamp))
+    fun printReport(expenses: List<Expense>, timestamp: Date = Date()) =
+        print(expenses.asSequence().generateReport(timestamp))
 
-    private fun List<Expense>.generateReport(timestamp: Date) = header(timestamp) + body() + summary()
+    private fun Sequence<Expense>.generateReport(timestamp: Date) = header(timestamp) + body() + summary()
 
     private fun header(timestamp: Date) = "Expenses $timestamp\n"
 
-    private fun List<Expense>.body() = joinToString(separator = "") { it.detail() }
+    private fun Sequence<Expense>.body() = joinToString(separator = "") { it.detail() }
     private fun Expense.detail() = "$name\t$amount\t$overLimitMarker\n"
     private val Expense.overLimitMarker get() = if (isOverLimit()) "X" else " "
 
-    private fun List<Expense>.summary() = "Meal expenses: ${sumMeals()}\nTotal expenses: ${sumAll()}\n"
-    private fun List<Expense>.sumAll() = sumOf { it.amount }
-    private fun List<Expense>.sumMeals() = filter { it.isMeal }.sumAll()
+    private fun Sequence<Expense>.summary() = "Meal expenses: ${sumMeals()}\nTotal expenses: ${sumAll()}\n"
+    private fun Sequence<Expense>.sumAll() = sumOf { it.amount }
+    private fun Sequence<Expense>.sumMeals() = filter { it.isMeal }.sumAll()
 }
--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -4,42 +4,32 @@ import com.nelkinda.training.ExpenseType.BREAKFAST
 import com.nelkinda.training.ExpenseType.CAR_RENTAL
 import com.nelkinda.training.ExpenseType.DINNER
 import com.nelkinda.training.ExpenseType.LUNCH
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 import java.util.Date
 
 class ExpenseReportTest {
-    private val originalStdout = System.out
-    private val interceptedStdout = ByteArrayOutputStream().also {
-        System.setOut(PrintStream(it))
-    }
-
-    @AfterEach
-    fun `restore stdout`() = System.setOut(originalStdout)
-
     @Test
     fun `characterize printReport`() {
         val now = Date()
-        ExpenseReport().printReport(
-            listOf(
-                Expense(BREAKFAST, 1),
-                Expense(BREAKFAST, 1000),
-                Expense(BREAKFAST, 1001),
-                Expense(DINNER, 2),
-                Expense(DINNER, 5000),
-                Expense(DINNER, 5001),
-                Expense(CAR_RENTAL, 4),
-                Expense(LUNCH, 8),
-                Expense(LUNCH, 2000),
-                Expense(LUNCH, 2001),
-            ),
-            now,
-        )
-        val actual = interceptedStdout.toString()
-        val expected = "Expenses $now\n" +
+        val actualReport = interceptStdout {
+            ExpenseReport().printReport(
+                listOf(
+                    Expense(BREAKFAST, 1),
+                    Expense(BREAKFAST, 1000),
+                    Expense(BREAKFAST, 1001),
+                    Expense(DINNER, 2),
+                    Expense(DINNER, 5000),
+                    Expense(DINNER, 5001),
+                    Expense(CAR_RENTAL, 4),
+                    Expense(LUNCH, 8),
+                    Expense(LUNCH, 2000),
+                    Expense(LUNCH, 2001),
+                ),
+                now,
+            )
+        }
+        val expectedReport = "Expenses $now\n" +
                 "Breakfast\t1\t \n" +
                 "Breakfast\t1000\t \n" +
                 "Breakfast\t1001\tX\n" +
@@ -52,6 +42,6 @@ class ExpenseReportTest {
                 "Lunch\t2001\tX\n" +
                 "Meal expenses: 16014\n" +
                 "Total expenses: 16018\n"
-        assertEquals(expected, actual)
+        assertEquals(expectedReport, actualReport)
     }
 }
