794e4c2575bb625d54c9eefa7ce1cdc5ea48181d
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-06-22 11:11:07 +0530

    Improve Pascal solution.

--- expensereport-pascal/ExpenseReport.pas
+++ expensereport-pascal/ExpenseReport.pas
@@ -17,7 +17,9 @@ CONST
     Dinner:    ExpenseType = ( name: 'Dinner';     limit:  5000; isMeal: true;  );
     Breakfast: ExpenseType = ( name: 'Breakfast';  limit:  1000; isMeal: true;  );
     CarRental: ExpenseType = ( name: 'Car Rental'; limit: 32767; isMeal: false; );
-    Lunch:     ExpenseType = ( name: 'Lunch';      limit:  2000; isMeal: true; );
+    Lunch:     ExpenseType = ( name: 'Lunch';      limit:  2000; isMeal: true;  );
+    TAB: STRING = #9;
+    NL: STRING = #10;
 
 FUNCTION isMeal(exp: Expense): BOOLEAN;
 BEGIN
@@ -54,47 +56,57 @@ BEGIN
     END;
 END;
 
-PROCEDURE printHeader();
+FUNCTION header(): STRING;
 BEGIN
-    writeln('Expenses: ', FormatDateTime('YYYY-MM-DD hh:mm:ss', Now));
+    header := 'Expenses: ' + FormatDateTime('YYYY-MM-DD hh:mm:ss', Now) + NL;
 END;
 
-FUNCTION overLimitMarker(exp: Expense): string;
+FUNCTION overLimitMarker(exp: Expense): STRING;
 BEGIN
     IF (isOverLimit(exp)) THEN overLimitMarker := 'X' ELSE overLimitMarker := ' ';
 END;
 
-PROCEDURE printDetails(expenses: ARRAY OF Expense);
+FUNCTION detail(exp: Expense): STRING;
+BEGIN
+    detail := getName(exp) + TAB + IntToStr(exp.amount) + TAB + overLimitMarker(exp) + NL;
+END;
+
+FUNCTION details(expenses: ARRAY OF Expense): STRING;
 VAR exp: Expense;
 BEGIN
+    details := '';
     FOR exp IN expenses DO
     BEGIN
-        writeln(getName(exp), #9, exp.amount, #9, overLimitMarker(exp));
+        details := details + detail(exp);
     END;
 END;
 
-PROCEDURE printSummary(expenses: ARRAY OF Expense);
+FUNCTION summary(expenses: ARRAY OF Expense): STRING;
 BEGIN
-    writeln('Meal expenses: ', sumMeals(expenses));
-    writeln('Total expenses: ', sumTotal(expenses));
+    summary :=
+        'Meal expenses: ' + IntToStr(sumMeals(expenses)) + NL +
+        'Total expenses: ' + IntToStr(sumTotal(expenses)) + NL;
+END;
+
+FUNCTION report(expenses: ARRAY OF Expense): STRING;
+BEGIN
+    report := header() + details(expenses) + summary(expenses);
 END;
 
 PROCEDURE printReport(expenses: ARRAY OF Expense);
 BEGIN
-    printHeader();
-    printDetails(expenses);
-    printSummary(expenses);
+    write(report(expenses));
 END;
 
 
 VAR expenses: ARRAY [1 .. 7] OF Expense;
 BEGIN
-    expenses[1].type_ := @Dinner; expenses[1].amount := 5000;
-    expenses[2].type_ := @Dinner; expenses[2].amount := 5001;
+    expenses[1].type_ := @Dinner;    expenses[1].amount := 5000;
+    expenses[2].type_ := @Dinner;    expenses[2].amount := 5001;
     expenses[3].type_ := @Breakfast; expenses[3].amount := 1000;
     expenses[4].type_ := @Breakfast; expenses[4].amount := 1001;
-    expenses[5].type_ := @CarRental; expenses[5].amount := 4;
-    expenses[6].type_ := @Lunch; expenses[6].amount := 2000;
-    expenses[7].type_ := @Lunch; expenses[7].amount := 2001;
+    expenses[5].type_ := @CarRental; expenses[5].amount :=    4;
+    expenses[6].type_ := @Lunch;     expenses[6].amount := 2000;
+    expenses[7].type_ := @Lunch;     expenses[7].amount := 2001;
     printReport(expenses);
 END.
