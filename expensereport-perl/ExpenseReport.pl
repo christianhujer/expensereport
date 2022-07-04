cd5add5a148c76988013f5e6cf98f303d4c28994
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-07-04 22:16:59 +0530

    Add Perl solution.

--- expensereport-perl/ExpenseReport.pl
+++ expensereport-perl/ExpenseReport.pl
@@ -1,50 +1,101 @@
 #!/usr/bin/perl
 
-package Expense;
+package ExpenseType;
+
+sub new {
+    my $class = shift;
+    my $self = {
+        'name' => shift,
+        'limit' => shift,
+        'isMeal' => shift,
+    };
+    bless $self, $class;
+    $self;
+}
 
 use constant {
-    DINNER => 1,
-    BREAKFAST => 2,
-    CAR_RENTAL => 3,
+    DINNER      => new ExpenseType("Dinner",        5000,   1),
+    BREAKFAST   => new ExpenseType("Breakfast",     1000,   1),
+    CAR_RENTAL  => new ExpenseType("Car Rental",   ~0>>1,   0),
+    LUNCH       => new ExpenseType("Lunch",         2000,   1),
 };
+
+
+package Expense;
 
 sub new {
     my $class = shift;
     my $self = {
         'type' => shift,
-        'amount' => shift
+        'amount' => shift,
     };
     bless $self, $class;
-    return $self;
+    $self;
 }
+
+sub getName() {
+    shift->{'type'}->{'name'};
+}
+
+sub isMeal() {
+    shift->{'type'}->{'isMeal'};
+}
+
+sub isOverLimit() {
+    my $self = shift;
+    $self->{'amount'} > $self->{'type'}->{'limit'};
+}
+
+
+package ExpenseReport;
 
 sub printReport(@) {
-    my @expenses = @_;
-    my $mealExpenses = 0;
-    my $total = 0;
-    my $datestring = localtime();
-    print "Expenses: $datestring\n";
-    for my $expense (@expenses) {
-        if ($expense->{'type'} == DINNER or $expense->{'type'} == BREAKFAST) {
-            $mealExpenses += $expense->{'amount'};
-        }
-
-        my $expenseName = "";
-        if ($expense->{'type'} == DINNER) {
-            $expenseName = "Dinner";
-        } elsif ($expense->{'type'} == BREAKFAST) {
-            $expenseName = "Breakfast";
-        } elsif ($expense->{'type'} == CAR_RENTAL) {
-            $expenseName = "Car Rental";
-        }
-
-        my $mealOverExpensesMarker = $expense->{'type'} == DINNER && $expense->{'amount'} > 5000 || $expense->{'type'} == BREAKFAST && $expense->{'amount'} > 1000 ? "X" : " ";
-
-        print "$expenseName\t".$expense->{'amount'}."\t$mealOverExpensesMarker\n";
-        $total += $expense->{'amount'};
-    }
-    print "Meal Expenses: $mealExpenses\n";
-    print "Total Expenses: $total\n";
+    print report(@_);
 }
 
-printReport(new Expense(CAR_RENTAL, 5), new Expense(DINNER, 5000), new Expense(BREAKFAST, 1001), new Expense(CAR_RENTAL, 4));
+sub report(@) {
+    header() . details(@_) . summary(@_);
+}
+
+sub header() {
+    "Expenses: ".localtime()."\n";
+}
+
+sub detail($) {
+    my $expense = shift;
+    my $mealOverExpensesMarker = $expense->isOverLimit() ? "X" : " ";
+    $expense->getName()."\t".$expense->{'amount'}."\t$mealOverExpensesMarker\n";
+}
+
+sub details(@) {
+    my $details = "";
+    $details .= detail($_) for @_;
+    $details;
+}
+
+sub summary(@) {
+    "Meal Expenses: ".sumMeals(@_)."\n".
+    "Total Expenses: ".sumTotal(@_)."\n";
+}
+
+sub sumMeals(@) {
+    my $mealExpenses = 0;
+    $mealExpenses += $_->{'amount'} * $_->isMeal() for @_;
+    $mealExpenses;
+}
+
+sub sumTotal(@) {
+    my $total = 0;
+    $total += $_->{'amount'} for @_;
+    $total;
+}
+
+printReport(
+    new Expense(ExpenseType::DINNER, 5000),
+    new Expense(ExpenseType::DINNER, 5001),
+    new Expense(ExpenseType::BREAKFAST, 1000),
+    new Expense(ExpenseType::BREAKFAST, 1001),
+    new Expense(ExpenseType::CAR_RENTAL, 4),
+    new Expense(ExpenseType::LUNCH, 2000),
+    new Expense(ExpenseType::LUNCH, 2001),
+);
