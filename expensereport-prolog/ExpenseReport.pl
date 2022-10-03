daae2aa3a73bbbfe636f492e8b396d21877ef485
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-10-03 16:18:19 +0530

    Significantly improve Prolog solution.

--- expensereport-prolog/ExpenseReport.pl
+++ expensereport-prolog/ExpenseReport.pl
@@ -10,54 +10,61 @@ is_meal(lunch).
 
 is_meal(Expense) :- expense(Expense, Type, _), is_meal(Type).
 is_over_limit(Expense) :- expense(Expense, Type, Amount), expense_type(Type, _, Limit), Amount > Limit.
+get_amount(Expense, Amount) :- expense(Expense, _, Amount).
 
 print_report(Expenses) :-
-    print_header(),
-    print_details(Expenses),
-    print_summary(Expenses).
+    generate_report(Expenses, Report),
+    write(Report).
 
-print_header() :-
+generate_report(Expenses, Report) :-
+    report_header(Header),
+    report_details(Expenses, Details),
+    report_summary(Expenses, Summary),
+    atomic_list_concat([Header, Details, Summary], '', Report).
+
+report_header(Header) :-
     get_time(Time),
     stamp_date_time(Time, Date, 'UTC'),
     format_time(atom(TS), '%FT%T%z', Date, posix),
-    format('Expenses: ~a~n', [TS]).
+    format(string(Header), 'Expenses: ~a~n', [TS]).
 
-print_details([Expense|Expenses]) :-
+report_details(Expenses, Details) :-
+    maplist(report_detail, Expenses, Detail),
+    atomic_list_concat(Detail, '', Details).
+
+report_detail(Expense, Detail) :-
     expense(Expense, Type, Amount),
     expense_type(Type, Name, _),
     is_over_limit_marker(Expense, Marker),
-    format('~a\t~a\t~a~n', [Name, Amount, Marker]),
-    print_details(Expenses).
-print_details([]).
+    format(string(Detail), '~a\t~a\t~a~n', [Name, Amount, Marker]).
 
 is_over_limit_marker(Expense, 'X') :- is_over_limit(Expense).
 is_over_limit_marker(Expense, ' ') :- not(is_over_limit(Expense)).
 
-print_summary(Expenses) :-
-    print_meal_expenses(Expenses),
-    print_total_expenses(Expenses).
+report_summary(Expenses, Summary) :-
+    sum_meals(Meals, Expenses),
+    sum_total(Total, Expenses),
+    format(string(Summary), 'Meal expenses: ~a~nTotal expenses: ~a~n', [Meals, Total]).
 
-print_meal_expenses([Expense|Expenses], Meals) :-
-    is_meal(Expense),
-    expense(Expense, _, Amount),
-    NMeals is Meals + Amount,
-    print_meal_expenses(Expenses, NMeals).
-print_meal_expenses([Expense|Expenses], Meals) :-
-    not(is_meal(Expense)),
-    print_meal_expenses(Expenses, Meals).
-print_meal_expenses([], Meals) :-
-    format('Meal expenses: ~a~n', [Meals]).
-print_meal_expenses(Expenses) :- print_meal_expenses(Expenses, 0).
+sum_meals(Sum, Expenses) :- include(is_meal, Expenses, Meals), sum_total(Sum, Meals).
+sum_total(Sum, Expenses) :- maplist(get_amount, Expenses, Amounts), foldl(plus, Amounts, 0, Sum).
 
-print_total_expenses([Expense|Expenses], Total) :-
-    expense(Expense, _, Amount),
-    NTotal is Total + Amount,
-    print_total_expenses(Expenses, NTotal).
-print_total_expenses([], Total) :-
-    format('Total expenses: ~a~n', [Total]).
-print_total_expenses(Expenses) :- print_total_expenses(Expenses, 0).
+test() :-
+    sum_meals(0, []),
+    sum_total(0, []),
+    sum_meals(11, [[dinner, 1], [breakfast, 2], [car_rental, 4], [lunch, 8]]),
+    sum_total(15, [[dinner, 1], [breakfast, 2], [car_rental, 4], [lunch, 8]]),
+    is_over_limit([dinner, 5001]),
+    is_over_limit([breakfast, 1001]),
+    is_over_limit([lunch, 2001]),
+    not(is_over_limit([dinner, 5000])),
+    not(is_over_limit([breakfast, 1000])),
+    not(is_over_limit([lunch, 2000])),
+    not(is_over_limit([car_rental, inf])),
+    !.
 
 main() :-
+    test(),
     print_report([
         [dinner, 5000],
         [dinner, 5001],
