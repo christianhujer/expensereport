9254c29e4280a6a8e273828689df3e1d8dc21c08
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-10 11:41:04 +0530

    Add test for flagging an expense

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -7,12 +7,18 @@ import com.nelkinda.training.model.ExpenseType.DINNER
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import java.util.*
 
 class ExpenseReportTest {
 
+    private lateinit var expenseReport: ExpenseReport
+    @BeforeEach
+    fun setUp() {
+        expenseReport = ExpenseReport()
+    }
     @Test
-    fun printReport() {
+    fun `should print expense for breakfast and dinner`() {
         val expense1 = Expense(BREAKFAST, 1000)
 
         val expense2 = Expense(DINNER, 2000)
@@ -22,8 +28,22 @@ class ExpenseReportTest {
             "Meal expenses: 3000", "Total expenses: 3000", ""
         )
 
-        val expenseReport = ExpenseReport()
         val output = tapSystemOut { expenseReport.printReport(listOf(expense1, expense2)) }
+        val actualResult = output.split("\n")
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `should flag breakfast`() {
+        val expense1 = Expense(BREAKFAST, 1001)
+
+        val expectedResult = listOf(
+            "Expenses ${Date()}", "Breakfast\t1001\tX",
+            "Meal expenses: 1001", "Total expenses: 1001", ""
+        )
+
+        val output = tapSystemOut { expenseReport.printReport(listOf(expense1)) }
         val actualResult = output.split("\n")
 
         assertEquals(expectedResult, actualResult)
