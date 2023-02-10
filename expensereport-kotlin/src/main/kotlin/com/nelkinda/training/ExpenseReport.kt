3ac06489ad8e6d919180debeac80dafd81e2bae1
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-10 11:15:09 +0530

    Remove expenseName assignment
    Add stringName as the expense types name

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
@@ -4,7 +4,7 @@ import com.nelkinda.training.model.Expense
 import com.nelkinda.training.model.ExpenseType
 import java.util.Date
 
-class ExpenseReport{
+class ExpenseReport {
     fun printReport(expenses: List<Expense>) {
         var total = 0
         var mealExpenses = 0
@@ -16,15 +16,10 @@ class ExpenseReport{
                 mealExpenses += expense.amount
             }
 
-            val expenseName: String = when (expense.type) {
-                ExpenseType.DINNER -> "Dinner"
-                ExpenseType.BREAKFAST -> "Breakfast"
-                ExpenseType.CAR_RENTAL -> "Car Rental"
-            }
+            val mealOverExpensesMarker =
+                if (expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000) "X" else " "
 
-            val mealOverExpensesMarker = if (expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000) "X" else " "
-
-            println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker)
+            println(expense.type.stringName + "\t" + expense.amount + "\t" + mealOverExpensesMarker)
 
             total += expense.amount
         }
--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/model/ExpenseType.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/model/ExpenseType.kt
@@ -1,5 +1,5 @@
 package com.nelkinda.training.model
 
-enum class ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+enum class ExpenseType(val stringName: String) {
+    DINNER("Dinner"), BREAKFAST("Breakfast"), CAR_RENTAL("Car Rental")
 }
