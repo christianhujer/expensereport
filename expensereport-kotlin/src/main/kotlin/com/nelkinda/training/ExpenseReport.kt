5f2a0efe9ce33f2e19de9684442ae69540eb6979
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-10 13:49:07 +0530

    Replace Pair for FinalExpense

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
@@ -3,15 +3,16 @@ package com.nelkinda.training
 import com.nelkinda.training.model.Expense
 import com.nelkinda.training.model.ExpenseType
 import com.nelkinda.training.model.ExpenseType.*
+import com.nelkinda.training.model.FinalExpense
 import java.util.Date
 
 class ExpenseReport {
     fun printReport(expenses: List<Expense>) {
         println("Expenses ${Date()}")
 
-        val pair1 = getTotalAndMealExpenses(expenses)
-        val mealExpenses = pair1.first
-        val total = pair1.second
+        val finalExpense = getTotalAndMealExpenses(expenses)
+        val mealExpenses = finalExpense.mealExpense
+        val total = finalExpense.total
 
         println("Meal expenses: $mealExpenses")
         println("Total expenses: $total")
@@ -19,7 +20,7 @@ class ExpenseReport {
 
     fun getTotalAndMealExpenses(
         expenses: List<Expense>
-    ): Pair<Int, Int> {
+    ): FinalExpense {
         var mealExpenses = 0
         var total = 0
 
@@ -33,7 +34,7 @@ class ExpenseReport {
 
             total += expense.amount
         }
-        return Pair(mealExpenses, total)
+        return FinalExpense(mealExpenses, total)
     }
 
 }
---
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/model/FinalExpense.kt
@@ -0,0 +1,3 @@
+package com.nelkinda.training.model
+
+data class FinalExpense(val mealExpense: Int, val total: Int)
