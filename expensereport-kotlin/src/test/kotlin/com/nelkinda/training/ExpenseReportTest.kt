0ee87b7b925bc344d0411b0b7e2fa3945d319b2b
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-10 12:18:36 +0530

    Add test for multiple expenses with some being flagged

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -2,8 +2,7 @@ package com.nelkinda.training
 
 import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
 import com.nelkinda.training.model.Expense
-import com.nelkinda.training.model.ExpenseType.BREAKFAST
-import com.nelkinda.training.model.ExpenseType.DINNER
+import com.nelkinda.training.model.ExpenseType.*
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -44,6 +43,22 @@ class ExpenseReportTest {
         )
 
         val output = tapSystemOut { expenseReport.printReport(listOf(expense1)) }
+        val actualResult = output.split("\n")
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `should flag the first expense and not the second`(){
+        val expense1 = Expense(BREAKFAST, 1001)
+        val expense2 = Expense(CAR_RENTAL, 2000)
+
+        val expectedResult = listOf(
+            "Expenses ${Date()}", "Breakfast\t1001\tX", "Car Rental\t2000\t ",
+            "Meal expenses: 1001", "Total expenses: 3001", ""
+        )
+
+        val output = tapSystemOut { expenseReport.printReport(listOf(expense1, expense2)) }
         val actualResult = output.split("\n")
 
         assertEquals(expectedResult, actualResult)
