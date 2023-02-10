5a0922ad2d27a7cb8b43c291b8f04805dc2a4f98
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-10 11:43:10 +0530

    Add Limit associated with the expense type

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/model/ExpenseType.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/model/ExpenseType.kt
@@ -1,5 +1,5 @@
 package com.nelkinda.training.model
 
-enum class ExpenseType(val stringName: String) {
-    DINNER("Dinner"), BREAKFAST("Breakfast"), CAR_RENTAL("Car Rental")
+enum class ExpenseType(val stringName: String, val expenseLimit: Int) {
+    DINNER("Dinner", 5000), BREAKFAST("Breakfast", 1000), CAR_RENTAL("Car Rental", 0)
 }
