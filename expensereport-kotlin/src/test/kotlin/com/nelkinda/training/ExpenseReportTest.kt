55dc6eb584bc0454d28dba591d0b315285521a6c
MaheshAravindV <maheshv@sahaj.ai>
2023-02-10 12:30:25 +0530

    Add approval test scenarios
    Expert has to mark tests as approved if it works as expected

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -35,7 +35,17 @@ class ExpenseReportTest {
         @JvmStatic
         fun inputForExpenseReport(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(emptyList<Expense>(), "output when no input is given")
+                Arguments.of(emptyList<Expense>(), "output when there is one breakfast expense costing 100"),
+                Arguments.of(listOf(Expense(ExpenseType.BREAKFAST, 100)), "output when there is one breakfast expense costing 100"),
+                Arguments.of(listOf(Expense(ExpenseType.DINNER, 100)), "output when there is one dinner expense costing 100"),
+                Arguments.of(listOf(Expense(ExpenseType.CAR_RENTAL, 100)), "output when there is one car rental expense costing 100"),
+                Arguments.of(listOf(Expense(ExpenseType.CAR_RENTAL, 100), Expense(ExpenseType.BREAKFAST, 200), Expense(ExpenseType.CAR_RENTAL, 500)), "output when there are multiple expenses"),
+                Arguments.of(listOf(Expense(ExpenseType.DINNER, 5000)), "output when there is one dinner expense at the expense limit border"),
+                Arguments.of(listOf(Expense(ExpenseType.DINNER, 10000)), "output when there is one dinner expense costing more than the expense limit"),
+                Arguments.of(listOf(Expense(ExpenseType.BREAKFAST, 1000)), "output when there is one breakfast expense at the expense limit border"),
+                Arguments.of(listOf(Expense(ExpenseType.BREAKFAST, 2000)), "output when there is one breakfast expense costing more than the expense limit"),
+                Arguments.of(listOf(Expense(ExpenseType.BREAKFAST, 2000), Expense(ExpenseType.DINNER, 10000)), "output when there are multiple expenses costing more than the expense limit"),
+                Arguments.of(listOf(Expense(ExpenseType.BREAKFAST, 2000), Expense(ExpenseType.BREAKFAST, 2000)), "output when there are multiple expenses of the same type"),
             )
         }
     }
@@ -44,6 +54,10 @@ class ExpenseReportTest {
     @MethodSource("inputForExpenseReport")
     fun `approval test`(expenses: List<Expense>, testName: String) {
         expenseReport.printReport(expenses)
-        Approvals.verifyAll("Output when no input is given", printStream.capturedOutput, Options().forFile().withBaseName(testName))
+        Approvals.verifyAll(
+            testName,
+            printStream.capturedOutput,
+            Options().forFile().withBaseName(testName)
+        )
     }
 }
