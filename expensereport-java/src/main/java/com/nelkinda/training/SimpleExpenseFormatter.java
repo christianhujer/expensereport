9709937606cad61425d78c9e473c5e20913e75f8
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2025-05-09 10:05:25 +0530

    refactor: Delegate formatting logic to ExpenseType in SimpleExpenseFormatter

--- expensereport-java/src/main/java/com/nelkinda/training/SimpleExpenseFormatter.java
+++ expensereport-java/src/main/java/com/nelkinda/training/SimpleExpenseFormatter.java
@@ -4,16 +4,8 @@ public class SimpleExpenseFormatter implements ExpenseFormatter {
 
     @Override
     public String format(Expense expense) {
-        String expenseName = switch (expense.type) {
-            case DINNER -> "dinner";
-            case BREAKFAST -> "breakfast";
-            case CAR_RENTAL -> "car rental";
-        };
-
-        String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 ||
-                                        expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
-
-        return String.format("%s %d %s", expenseName, expense.amount, mealOverExpensesMarker);
+        String mealOverExpensesMarker = expense.exceedsLimit() ? "X" : " ";
+        return String.format("%s %d %s", expense.getName(), expense.amount(), mealOverExpensesMarker);
     }
 
     @Override
