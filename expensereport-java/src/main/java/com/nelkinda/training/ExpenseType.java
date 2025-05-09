c4fdce7dbc47397f7f7f1444c1b31a5003eb0e6d
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2025-05-09 10:04:42 +0530

    refactor: Replace switch and if statements with polymorphism in ExpenseType

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseType.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseType.java
@@ -1,5 +1,58 @@
 package com.nelkinda.training;
 
-public enum ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+public abstract class ExpenseType {
+    public abstract String getName();
+    public abstract boolean isMeal();
+    public abstract boolean exceedsLimit(int amount);
+
+    public static final ExpenseType DINNER = new ExpenseType() {
+        @Override
+        public String getName() {
+            return "dinner";
+        }
+
+        @Override
+        public boolean isMeal() {
+            return true;
+        }
+
+        @Override
+        public boolean exceedsLimit(int amount) {
+            return amount > 5000;
+        }
+    };
+
+    public static final ExpenseType BREAKFAST = new ExpenseType() {
+        @Override
+        public String getName() {
+            return "breakfast";
+        }
+
+        @Override
+        public boolean isMeal() {
+            return true;
+        }
+
+        @Override
+        public boolean exceedsLimit(int amount) {
+            return amount > 1000;
+        }
+    };
+
+    public static final ExpenseType CAR_RENTAL = new ExpenseType() {
+        @Override
+        public String getName() {
+            return "car rental";
+        }
+
+        @Override
+        public boolean isMeal() {
+            return false;
+        }
+
+        @Override
+        public boolean exceedsLimit(int amount) {
+            return false; // No limit for car rental
+        }
+    };
 }
